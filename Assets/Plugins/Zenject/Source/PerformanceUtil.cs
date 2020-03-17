f5d72ab0e3c0d6fc7a84bf46d45725f651c6005d
chromee <odk.mmmm@gmail.com>
2020-03-17 17:28:53 +0900

    first commit

---
+++ Assets/Plugins/Zenject/Source/PerformanceUtil.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace Zenject
+{
+    public class PerformanceUtil
+    {
+        private static readonly string Path = $"{Application.dataPath}/../output.log";
+
+        public static void WriteFile(string msg)
+        {
+            using (var file = new StreamWriter(Path, true))
+            {
+                file.WriteLine(msg);
+            }
+        }
+    }
+}
